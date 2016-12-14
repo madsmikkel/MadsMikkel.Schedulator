5216868291559d595247697cd53c8e497b4c51a9
Mads Mikkel Rasmussen <mads.mikkel@outlook.com>
2016-12-14 08:40:30 +0100

    Added more to core.

---
+++ MadsMikkel.Schedulator.Core/Train.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadsMikkel.Schedulator.Core
+{
+	public class Train
+	{
+		public TrainSchedule Schedule { get; set; }
+	}
+}
